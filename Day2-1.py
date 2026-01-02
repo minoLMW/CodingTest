52b3d6d4046b9723736ed2490194708aa9c96646
minoLMW <minwoo552@gmail.com>
2026-01-02 15:36:05 +0900

    init

---
+++ Day2-1.py
@@ -0,0 +1,112 @@
+s = "Hello123World"
+
+def analyze_string(s):
+    digit_count = 0
+    lower_count = 0
+    upper_count = 0
+
+    for ch in s:
+        if ch.isdigit():
+            digit_count += 1
+        elif ch.islower():
+            lower_count += 1
+        elif ch.isupper():
+            upper_count += 1
+
+    print("숫자:", digit_count)
+    print("소문자:", lower_count)
+    print("대문자:", upper_count)
+
+s = "Hello123World"
+analyze_string(s)
+
+arr = [1, 2, 3, 4, 5]
+
+def transform_numbers(arr):
+    result = []
+
+    for num in arr:
+        if num % 2 == 0:
+            result.append(num)
+        else:
+            result.append(num * 2)
+
+    return result
+
+arr = [1, 2, 3, 4, 5]
+print(transform_numbers(arr))
+
+s = "Hello123World"
+
+def analyze_string(s):
+    digit_count = 0
+    lower_count = 0
+    upper_count = 0
+
+    for ch in s:
+        if ch.isdigit():
+            digit_count += 1
+        elif ch.islower():
+            lower_count += 1
+        elif ch.isupper():
+            upper_count += 1
+
+    print("숫자:", digit_count)
+    print("소문자:", lower_count)
+    print("대문자:", upper_count)
+
+s = "Hello123World"
+analyze_string(s)
+
+arr = [1, 2, 3, 4, 5]
+
+def transform_numbers(arr):
+    result = []
+
+    for num in arr:
+        if num % 2 == 0:
+            result.append(num)
+        else:
+            result.append(num * 2)
+    return result
+
+arr = [1, 2, 3, 4, 5]
+print(transform_numbers(arr))
+
+s = "Hello123World"
+
+def analyze_string(s):
+    digit_count = 0
+    lower_count = 0
+    upper_count = 0
+
+    for ch in s:
+        if ch.isdigit():
+            digit_count += 1
+        elif ch.islower():
+            lower_count += 1
+        elif ch.isupper():
+            upper_count += 1
+
+    print("숫자:", digit_count)
+    print("소문자:", lower_count)
+    print("대문자:", upper_count)
+
+s = "Hello123World"
+analyze_string(s)
+
+arr = [1, 2, 3, 4, 5]
+
+def transform_numbers(arr):
+    result = []
+    
+    for num in arr:
+        if num % 2 == 0:
+            result.append(num)
+        else:
+            result.append(num * 2)
+
+    return result
+
+arr = [1, 2, 3, 4, 5]
+print(transform_numbers(arr))
